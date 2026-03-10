4e77605e2b96dcff2feaf28680a90c05188c3562
Inna Tablizo <tablizo.innabsemc2023@gmail.com>
2026-03-10 19:43:44 +0800

    bagong underscore puta

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar_menu')
+const menuLinks = document.querySelector('.navbar__menu')
 
 menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
@@ -7,7 +7,7 @@ menu.addEventListener('click', function(){
 })
 
 /* environment carousel */
-const cards = document.querySelectorAll(".carousel_card");
+const cards = document.querySelectorAll(".carousel__card");
 
 const dots = document.querySelectorAll(".dot");
 s
